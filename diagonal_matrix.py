3f05ecc9372b9ca813f94586d3e47c25939d117a
Grisha <parfen20@rambler.ru>
2022-07-08 15:21:15 +0200

    I did it

--- diagonal_matrix.py
+++ diagonal_matrix.py
@@ -2,13 +2,15 @@ n, m = [int(i) for i in input().split()]
 matrix = [[0]*m for i in range(n)]
 count = 1
 
-for i in range(m+1):
+for i in range(m+n):
 	for j in range(i):
 		for k in range(i):
-			if j+k == i-1 and j < n:
+			if j+k == i-1 and j < n and k < m:
 				matrix[j][k] = count
 				count += 1
 
 
-for i in matrix:
-	print(*i)
+for row in matrix:
+	for el in row:
+		print(str(el).ljust(3), end=' ')
+	print()
